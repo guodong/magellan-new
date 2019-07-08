184637c764cf4a53357d568367be2ba473cae236
czusual <1357017822@qq.com>
2019-07-08 21:02:22 +0800

    Update l2_offline.py
    import function from zoe1

--- apps/l2/l2_offline.py
+++ apps/l2/l2_offline.py
@@ -2,5 +2,6 @@ import sys, os
 sys.path.append(os.path.join(os.path.dirname(__file__), "../.."))
 from magellan import *
 import json
+from zoe1 import for_Dong #this function ruturn a dict{switchid{inner_portid:outportset}}
 
 set('macTable', json.dumps({'00:00:00:00:00:00': '1'}))
