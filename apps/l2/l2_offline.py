1b7956ed1510321f7036c7c3e90f21bcf7c362a4
guodong <gd@tongji.edu.cn>
2019-07-10 18:09:30 +0800

    fix set

--- apps/l2/l2_offline.py
+++ apps/l2/l2_offline.py
@@ -4,6 +4,5 @@ from magellan import *
 import json
 from zoe1.Magellan_STP_Zoe import for_Dong #this function ruturn a dict{switchid{inner_portid:outportset}}
 
-set('macTable', json.dumps({'00:00:00:00:00:00': '1'}))
-set('STP', json.dumps(for_Dong()[1]))
-print(for_Dong())
+# set('macTable', {'00:00:00:00:00:01': '1'})
+set('STP', for_Dong()[1])
--- magellan/__init__.py
+++ magellan/__init__.py
@@ -1,8 +1,9 @@
+import json
 from magellan_pb2 import *
 from magellan_pb2_grpc import *
 
 def set(varname, value):
     conn = grpc.insecure_channel('localhost:7777')
     client = gRPCStub(channel=conn)
-    client.SetVariable(Variable(name=varname, value=value))
+    client.SetVariable(Variable(name=varname, value=json.dumps(value)))
 
